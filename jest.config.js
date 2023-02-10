d071a6e699c03aecfa2ab9bb75ffd25e20825596
Thiago Puntar <thiagopuntar@gmail.com>
2023-02-10 15:05:12 -0300

    fix: setup basic jest config

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,6 @@ const createJestConfig = nextJest({
 
 // Add any custom config to be passed to Jest
 const customJestConfig = {
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ["node_modules", "<rootDir>/"],
   testEnvironment: "jest-environment-jsdom",
