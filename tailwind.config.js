05b44f0a6485c5ab0ba42219faac7017511d3248
salviotonon <salvio.tonon@gmail.com>
2023-02-10 19:59:48 -0300

    fix(fix): fix yarn lock

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        poppins: ["Poppins", "sans-serif"],
+        sans: ["Poppins", "sans-serif"],
       },
       colors: {
         primary: {
