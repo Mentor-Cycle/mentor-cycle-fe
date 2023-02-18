cd344e9ad748be138da5ce2bc591a71f3154299d
Leonardo Balsalobre <oliveirabalsa2@gmail.com>
2023-02-18 13:11:40 -0300

    feat: add chip component

---
+++ src/pages/theo-task.tsx
@@ -0,0 +1,13 @@
+import Chip, { ChipOption } from "@components/Chip";
+
+function TheoTest() {
+  return (
+    <main className="w-screen h-screen flex gap-5 justify-center items-center">
+      <Chip type={ChipOption.BACKEND} />
+      <Chip type={ChipOption.FRONTEND} />
+      <Chip type={ChipOption.FULLSTACK} />
+    </main>
+  );
+}
+
+export default TheoTest;
