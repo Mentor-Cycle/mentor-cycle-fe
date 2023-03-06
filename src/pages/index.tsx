4ffd53049e95c4663ef7e37c29b2c2b36a543492
Leonardo <oliveirabalsa2@gmail.com>
2023-03-05 22:17:42 -0300

    feat: create temporary landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,68 @@
 import type { NextPage } from "next";
+import Image from "next/image";
+import logo from "../public/logo-white.svg";
+import notion from "../public/notion.svg";
+import linkedin from "../public/linkedin.svg";
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <main className="flex flex-col items-center py-16 px-0 flex-1 min-h-screen">
-        <h1 className="text-primary-05 text-5xl">style/theme</h1>
-        <h1 className="text-primary-04 text-5xl">style/theme</h1>
-        <h1 className="text-primary-03 text-5xl">style/theme</h1>
-        <h1 className="text-primary-02 text-5xl">style/theme</h1>
-        <h1 className="text-primary-01 text-5xl">style/theme</h1>
-        <p className="text-gray-05 text-3xl">01</p>
-        <p className="text-gray-04 text-3xl">02</p>
-        <p className="text-gray-03 text-3xl">03</p>
-        <p className="text-gray-02 text-3xl">04</p>
-        <p className="text-gray-01 text-3xl">05</p>
-        <div className="bg-primary-05 p-4">
-          <p className="text-neutral-05 text-3xl">01</p>
-          <p className="text-neutral-04 text-3xl">02</p>
-          <p className="text-neutral-03 text-3xl">03</p>
-          <p className="text-neutral-02 text-3xl">04</p>
-          <p className="text-neutral-01 text-3xl">05</p>
-        </div>
-        <button className="bg-link-02 text-neutral-04 px-4 py-2">button</button>
-      </main>
-    </div>
+    <main
+      className="w-full h-screen bg-gradient-to-r from-[#212324] to-[#DE032A]
+    "
+    >
+      <article
+        className=" max-w-sm mx-auto w-full h-screen flex flex-col items-center justify-center 
+    "
+      >
+        <Image src={logo} width={300} height={300} alt="Mentor Cycle Logo" />
+        <h1 className="text-colors-neutral-01 text-5xl font-normal mt-16">
+          Em Breve
+        </h1>
+        <p className="mt-10 text-colors-neutral-01 text-base">
+          Tire suas dúvidas de forma rápida e prática. Construa seu futuro sem
+          dúvidas.
+        </p>
+        <p className="mt-12 text-colors-neutral-01 text-base">
+          Conheça melhor o projeto em nossas redes:
+        </p>
+        <section className="mt-10 w-40 flex justify-between items-center">
+          {/* linkedin and notion */}
+          <a
+            href="https://www.linkedin.com/company/mentorcycle/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Image
+              src={linkedin}
+              width={30}
+              height={30}
+              alt="Linkedin Mentor Cycle"
+            />
+          </a>
+          <a
+            href="https://uxdavidareal.notion.site/Mentor-Cycle-ff5e6e73778d40d690d57aeac24d8cb5"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Image
+              src={notion}
+              width={30}
+              height={30}
+              alt="Notion Mentor Cycle"
+            />
+          </a>
+        </section>
+        <a
+          href="https://www.figma.com/file/KnsardnDQ2lDKUYo58G8Pf/Mentor-Cycle?node-id=240%3A197&t=yyUDX06BedyrhAgE-1"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button className="mt-10 py-3 px-10 rounded-lg text-colors-neutral-02 bg-colors-secondary-03 font-bold text-xl">
+            Veja o esboço inicial do projeto
+          </button>
+        </a>
+      </article>
+    </main>
   );
 };
 
