e3fa7e9e8aa56b5abc73574ad8978c10a19416d5
salviotonon <salvio.tonon@gmail.com>
2023-03-13 22:32:13 -0300

    refactor: delete component homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,6 @@ import StepperVertical from "@components/StepperVertical";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return (
-    <div className="min-h-screen m-auto flex items-center justify-center max-w-lg">
-      <StepperVertical />
-    </div>
-  );
+  return <h1>hello world</h1>;
 };
 export default Home;
