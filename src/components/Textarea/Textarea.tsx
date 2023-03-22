042a662be380cc99beb7871eca8b16879ecafb4e
Matheus Gabriel <matheus@clientbase.com.br>
2023-03-22 10:42:39 -0300

    feat: textarea style

--- src/components/Textarea/Textarea.tsx
+++ src/components/Textarea/Textarea.tsx
@@ -14,10 +14,7 @@ const Textarea = ({ label, name, disabled, ...props }: TextareaProps) => {
 
   return (
     <Label.Root
-      className={clsx(
-        disabled && "text-gray-03 hover:text-gray-03 dark:opacity-50",
-        "text-secondary-04 hover:text-secondary-02 dark:text-neutral-01 font-semibold"
-      )}
+      className={clsx(disabled && "input-label-disabled", "input-label")}
       htmlFor={name}
     >
       {label}
@@ -27,16 +24,13 @@ const Textarea = ({ label, name, disabled, ...props }: TextareaProps) => {
         {...props}
         className={clsx(
           "w-full h-48 resize-none",
-          invalid &&
-            "invalid:border-danger-01 invalid:focus:ring-1 invalid:focus:ring-danger-01 invalid:text-danger-01 invalid:placeholder:text-danger-01 dark:invalid:placeholder:text-danger-01",
-          " font-normal text-gray-03 border border-gray-03 px-6 py-4 rounded-lg my-2 placeholder:text-gray-03 cursor-pointer focus:outline-none focus:ring-1 focus:ring-gray-05 transition-colors bg-neutral-03 hover:bg-neutral-01 active:placeholder:text-secondary-01 active:text-secondary-01 disabled:cursor-not-allowed disabled:opacity-50 disabled:active:placeholder:text-gray-03 dark:bg-secondary-03 dark:border-neutral-01 dark:placeholder:text-neutral-01 dark:text-neutral-01 dark:hover:bg-secondary-01 dark:active:bg-secondary-05 dark:focus:ring-1 dark:focus:ring-neutral-01 dark:transition-colors dark:disabled:bg-gray-02 dark:disabled:active:text-current dark:disabled:active:placeholder:text-current"
+          invalid && "input-invalid",
+          "input-default"
         )}
         onBlur={handleBlur}
       />
       {errorMessage && !disabled && (
-        <div className={"font-normal my-2 text-danger-01 text-sm"}>
-          {errorMessage}
-        </div>
+        <div className="input-error">{errorMessage}</div>
       )}
     </Label.Root>
   );
--- src/components/Textarea/Textarea.types.ts
+++ src/components/Textarea/Textarea.types.ts
@@ -2,11 +2,4 @@ export interface TextareaProps
   extends Omit<React.HTMLProps<HTMLTextAreaElement>, "size"> {
   name: string;
   label?: string;
-  required?: boolean;
-  pattern?: string;
-  minLength?: number;
-  maxLength?: number;
-  placeholder?: string;
-  type?: string;
-  disabled?: boolean;
 }
