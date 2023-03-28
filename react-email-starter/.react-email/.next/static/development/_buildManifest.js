3cc48a9df4f6419a13bb262c19b831a0004c0796
salviotonon <salvio.tonon@gmail.com>
2023-03-28 20:03:53 -0300

    fix: remove .next

--- react-email-starter/.react-email/.next/static/development/_buildManifest.js
+++
@@ -1,9 +0,0 @@
-self.__BUILD_MANIFEST = {
-  __rewrites: { beforeFiles: [], afterFiles: [], fallback: [] },
-  "/_error": ["static\u002Fchunks\u002Fpages\u002F_error.js"],
-  "/preview/[slug]": [
-    "static\u002Fchunks\u002Fpages\u002Fpreview\u002F[slug].js",
-  ],
-  sortedPages: ["\u002F_app", "\u002F_error", "\u002Fpreview\u002F[slug]"],
-};
-self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB();
