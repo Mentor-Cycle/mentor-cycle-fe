03ac703c4d2f8e99bf1d0b481a5d63fb9b23255e
Leonardo <oliveirabalsa2@gmail.com>
2023-04-05 17:18:20 -0300

    refactor: font and signin page

---
+++ src/pages/_document.tsx
@@ -0,0 +1,22 @@
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css?family=Poppins:400,500,600,700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -53,8 +53,8 @@ const SignIn: NextPage = () => {
           height={88}
           className="mx-auto md:mx-0 md:mt-22"
         />
-        <div className="flex flex-col justify-center max-w-[357px] 2xl:max-w-[500px] m-auto md:m-0 lg:mt-32 3xl:mt-96 3xl:m-auto">
-          <h2 className="text-neutral-01 text-3xl sm:text-4.5xl p-2 sm:p-0 sm:leading-11 font-bold text-center md:text-left md:mt-0 md:pt-40 2xl:text-6xl 3xl:pt-0 3xl:text-7xl ">
+        <div className="flex flex-col justify-center max-w-[357px] 2xl:max-w-[500px] m-auto md:m-0 lg:mt-32">
+          <h2 className="text-neutral-01 text-3xl sm:text-4.5xl p-2 sm:p-0 sm:leading-11 font-bold text-center md:text-left md:mt-0 md:pt-40">
             Comece o seu aprendizado por aqui!
           </h2>
           <p className="text-neutral-01 mt-2 text-sm sm:text-base text-center md:text-left p-2 sm:p-0 2xl:text-xl">
@@ -72,7 +72,7 @@ const SignIn: NextPage = () => {
         </div>
       </div>
       <div className="bg-neutral-01 col-span-1 md:col-span-6 py-16 md:px-8">
-        <div className="max-w-sm sm:max-w-md mx-auto px-4 sm:px-0 3xl:h-full 3xl:flex 3xl:flex-col 3xl:justify-center 3xl:max-w-[600px]">
+        <div className="max-w-sm sm:max-w-md mx-auto px-4 sm:px-0">
           <h1 className="mt-0 mb-4 text-primary-05 text-5xl text-center md:text-left dark:text-neutral-01">
             Bem-Vindo ao Mentor Cycle!
           </h1>
