d20f76a56864221278c385f6bdf4420f2c6f799e
Gabriel Shiota <gabrielnogueirashiota@gmail.com>
2023-04-12 15:55:40 -0300

    feat: create NavBar component for multiple actions

---
+++ src/components/NavBar/NavBar.types.ts
@@ -0,0 +1,10 @@
+type MenuClickActions = Record<string, () => void>;
+
+export type NavBarProps = {
+  itemsMenu: {
+    text: string;
+    action: keyof MenuClickActions;
+  }[];
+  menuClickActions: MenuClickActions;
+  className: string;
+};
