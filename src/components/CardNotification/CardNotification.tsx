37b6b607c536747d45422ae9a01e991523b9692a
Gabriel Shiota <gabrielnogueirashiota@gmail.com>
2023-04-14 15:23:03 -0300

    fix: add overflow ellipsis in text of description

--- src/components/CardNotification/CardNotification.tsx
+++ src/components/CardNotification/CardNotification.tsx
@@ -2,8 +2,9 @@ import Image from "next/image";
 
 import clsx from "clsx";
 import Link from "next/link";
-import { AiOutlineArrowRight } from "react-icons/ai";
 import { CardNotificationProps } from "./CardNotification.types";
+
+import { ImArrowRight2 } from "react-icons/im";
 
 const CardNotification = ({
   imgUrl,
@@ -15,22 +16,29 @@ const CardNotification = ({
     <Link href="">
       <div
         data-testid="teste"
-        className={clsx("bg-primary-01 rounded-lg px-4 py-5 flex w-full ", {
-          " bg-transparent border border-gray-03 ": alreadyViewed,
-        })}
+        className={clsx(
+          "bg-primary-01 rounded-lg px-4 py-5 flex w-full max-w-[634px] h-22 ",
+          {
+            " bg-transparent border border-gray-03 ": alreadyViewed,
+          }
+        )}
       >
         <Image src={imgUrl} alt="" width={48} height={48} />
         <div
-          className={clsx("flex flex-col ml-4  ", {
+          className={clsx("flex flex-col ml-4 h-full", {
             " text-gray-04": alreadyViewed,
             "text-neutral-01": !alreadyViewed,
           })}
         >
           <span className="self-start font-bold ">{name}</span>
-          <span className="text-sm flex items-center justify-center gap-1 ">
-            <AiOutlineArrowRight size={16} />
-            {description}
-          </span>
+          <div className="flex items-center gap-4">
+            <span>
+              <ImArrowRight2 />
+            </span>
+            <span className=" text-sm truncate max-w-[498px]">
+              {description}
+            </span>
+          </div>
         </div>
       </div>
     </Link>
