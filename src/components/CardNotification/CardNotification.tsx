5ce185f2ce3ec54d34f2b1924ac17269e5388648
Gabriel Shiota <gabrielnogueirashiota@gmail.com>
2023-04-13 23:26:43 -0300

    refactor: add a link around the content for redirect to notification

--- src/components/CardNotification/CardNotification.tsx
+++ src/components/CardNotification/CardNotification.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
 import clsx from "clsx";
+import Link from "next/link";
 import { AiOutlineArrowRight } from "react-icons/ai";
 import { CardNotificationProps } from "./CardNotification.types";
 
@@ -11,26 +12,28 @@ const CardNotification = ({
   alreadyViewed = false,
 }: CardNotificationProps) => {
   return (
-    <div
-      data-testid="teste"
-      className={clsx("bg-primary-01 rounded-lg px-4 py-5 flex w-full ", {
-        " bg-transparent border border-gray-03 ": alreadyViewed,
-      })}
-    >
-      <Image src={imgUrl} alt="" width={48} height={48} />
+    <Link href="">
       <div
-        className={clsx("flex flex-col ml-4  ", {
-          " text-gray-04": alreadyViewed,
-          "text-neutral-01": !alreadyViewed,
+        data-testid="teste"
+        className={clsx("bg-primary-01 rounded-lg px-4 py-5 flex w-full ", {
+          " bg-transparent border border-gray-03 ": alreadyViewed,
         })}
       >
-        <span className="self-start font-bold ">{name}</span>
-        <span className="text-sm flex items-center justify-center gap-1 ">
-          <AiOutlineArrowRight size={16} />
-          {description}
-        </span>
+        <Image src={imgUrl} alt="" width={48} height={48} />
+        <div
+          className={clsx("flex flex-col ml-4  ", {
+            " text-gray-04": alreadyViewed,
+            "text-neutral-01": !alreadyViewed,
+          })}
+        >
+          <span className="self-start font-bold ">{name}</span>
+          <span className="text-sm flex items-center justify-center gap-1 ">
+            <AiOutlineArrowRight size={16} />
+            {description}
+          </span>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
