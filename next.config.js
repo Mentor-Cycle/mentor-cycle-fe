8771162389b76978b5a3a3acad657b8bf66e2504
Matheus Silverio <matheusphillipo@gmail.com>
2023-04-17 17:07:05 -0300

    feat: config de suporte de imagens

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ["lh3.googleusercontent.com"],
+    formats: ["image/avif", "image/webp"],
+  },
   reactStrictMode: true,
   async rewrites() {
     return [
