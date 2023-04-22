a1891610986528098e79ea0f547008e2e4d2e553
Leonardo <oliveirabalsa2@gmail.com>
2023-04-22 11:01:42 -0300

    fix: remove duplicated import

--- src/providers/auth/auth.tsx
+++ src/providers/auth/auth.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect } from "react";
-import { useContext } from "react";
+import React, { useEffect, useContext } from "react";
 import { useRouter } from "next/router";
 import { initialValue, UserContext } from "providers/user/AppContext";
 import { GET_ME } from "services/apollo/querys";
