979800f1fdeaf00f7857c299cfebe81e267a7eb0
salviotonon <salvio.tonon@gmail.com>
2023-05-02 16:57:30 -0300

    chore: add Terms and Privacy policy

---
+++ src/components/TermsAndPrivacyPopup/TermsAndPrivacyPopup.tsx
@@ -0,0 +1,58 @@
+import { Dialog, DialogOverlay, DialogContent } from "@radix-ui/react-dialog";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+const TermsAndPrivacyPopup = () => {
+  const [dialogOpen, setDialogOpen] = useState(false);
+
+  useEffect(() => {
+    const hasAgreed = localStorage.getItem("hasAgreedToTermsAndPrivacy");
+    if (!hasAgreed) {
+      setDialogOpen(true);
+    }
+  }, []);
+
+  const handleAgree = () => {
+    localStorage.setItem("hasAgreedToTermsAndPrivacy", "true");
+    setDialogOpen(false);
+  };
+
+  return (
+    <Dialog open={dialogOpen}>
+      <DialogOverlay className="fixed z-10 inset-0 bg-secondary-01 bg-opacity-50" />
+      <DialogContent
+        onCloseAutoFocus={(e) => e.preventDefault()}
+        className="fixed z-20 bottom-[-10vh] left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-neutral-01 p-6 rounded shadow-lg"
+      >
+        <h2 className="text-xl font-bold mb-4">
+          Termos de Uso, Política de Privacidade e Cookies
+        </h2>
+        <p>
+          Ao utilizar esta plataforma, você concorda com nossos{" "}
+          <Link href="/terms" className="text-link-01 hover:text-link-02">
+            Termos de Uso
+          </Link>{" "}
+          e{" "}
+          <Link
+            href="/privacy-policy"
+            className="text-link-01 hover:text-link-02"
+          >
+            Política de Privacidade
+          </Link>
+          . Além disso, usamos cookies para melhorar sua experiência em nosso
+          site. Ao clicar em Concordo, você aceita o uso de cookies e nossos
+          termos.
+        </p>
+        <button
+          type="button"
+          className="mt-4 bg-blue-600 text-white px-4 py-2 rounded"
+          onClick={handleAgree}
+        >
+          Concordo
+        </button>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default TermsAndPrivacyPopup;
---
+++ src/pages/privacy-policy.tsx
@@ -0,0 +1,82 @@
+import { NextPage } from "next";
+
+const PrivacyPolicy: NextPage = () => {
+  return (
+    <div className="container py-10">
+      <h1 className="text-center font-bold text-3xl mt-8 mb-8">
+        Política de Privacidade da Mentor Cycle
+      </h1>
+      <p className="italic font-bold text-lg mb-8">1. Introdução</p>
+      <p className="mb-4">
+        A Mentor Cycle respeita a privacidade de seus usuários e está
+        comprometida em proteger suas informações pessoais. Esta Política de
+        Privacidade descreve como coletamos, usamos, compartilhamos e protegemos
+        suas informações pessoais quando você acessa e utiliza a plataforma
+        Mentor Cycle. Ao utilizar a plataforma, você concorda com a coleta e uso
+        de suas informações pessoais de acordo com esta Política de Privacidade.
+      </p>
+      <p className="italic font-bold text-lg mb-8">2. Coleta de informações</p>
+      <p className="mb-8">
+        Podemos coletar informações pessoais de diversas formas, incluindo:
+        Informações que você fornece durante o processo de registro, como nome,
+        e-mail, senha e outras informações relevantes; Informações que você
+        fornece ao usar a plataforma, como suas experiências, feedback e
+        interações com outros usuários; Informações de uso e acesso, como
+        endereço IP, tipo de navegador, idioma e horários de acesso.
+      </p>
+      <p className="italic font-bold text-lg mb-8">3. Uso das informações</p>
+      <p className="mb-4">
+        Utilizamos as informações coletadas para: Fornecer, manter e melhorar a
+        plataforma e nossos serviços; Personalizar a experiência do usuário,
+        como sugerir mentores e aprendizes com base em suas preferências e
+        necessidades; Comunicar com você sobre atualizações, ofertas e
+        notificações relevantes; Detectar, prevenir e solucionar problemas de
+        segurança e técnicos.
+      </p>
+      <p className="italic font-bold text-lg mb-8">
+        4. Compartilhamento de informações
+      </p>
+      <p className="mb-4">
+        A Mentor Cycle não vende, aluga suas informações pessoais com terceiros
+        sem o seu consentimento explícito. Podemos compartilhar suas informações
+        pessoais em situações específicas, como: Com prestadores de serviços que
+        nos ajudam a operar e melhorar a plataforma; Quando exigido por lei,
+        regulamentações ou ordens judiciais; Para proteger a segurança e
+        integridade da plataforma e dos direitos de outros usuários.
+      </p>
+      <h2 className="italic font-bold text-lg mb-8">
+        5. Segurança das informações
+      </h2>
+      <p className="mb-4">
+        A Mentor Cycle adota medidas de segurança razoáveis para proteger suas
+        informações pessoais contra acesso não autorizado, alteração, divulgação
+        ou destruição. No entanto, nenhum método de transmissão ou armazenamento
+        de informações é 100% seguro, e não podemos garantir a segurança
+        absoluta de suas informações pessoais.
+      </p>
+      <p className="italic font-bold text-lg mb-8">
+        6. Acesso e atualização de suas informações
+      </p>
+      <p className="mb-4">
+        Você pode acessar, atualizar ou excluir suas informações pessoais a
+        qualquer momento, acessando as configurações da sua conta na plataforma.
+        Se você tiver dificuldades em atualizar ou excluir suas informações,
+        entre em contato conosco para obter assistência.
+      </p>
+      <p className="italic font-bold text-lg mb-8">
+        7. Alterações nesta Política de Privacidade
+      </p>
+      <p>
+        A Mentor Cycle pode atualizar esta Política de Privacidade
+        periodicamente. Ao continuar utilizando a plataforma após a publicação
+        de alterações nesta Política de Privacidade, você concorda em se
+        vincular aos termos atualizados.
+      </p>
+      <p className="font-bold text-center mt-4">
+        Data da última atualização: 02 de maio de 2023
+      </p>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
