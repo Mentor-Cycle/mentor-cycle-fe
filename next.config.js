eab8bf1dec00fbd3ec16fcbd43ab812d29777750
Leonardo <oliveirabalsa2@gmail.com>
2023-06-01 00:47:23 -0300

    refactor: change url s3

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
       "www.gravatar.com",
       "localhost",
       "media.licdn.com",
-      "mentorcycle-dev.s3.us-east-2.amazonaws.com",
+      "mentor-cycle-prod.s3.us-east-2.amazonaws.com",
     ],
     formats: ["image/avif", "image/webp"],
   },
