a66a187014e46c9b2d235c4978d971e5e64ad301
Vitor Markis <vitormarkis2369@gmail.com>
2023-06-16 15:34:37 -0300

    feat: add GET_MENTORS query props and types to page

---
+++ src/schemas/mentor.ts
@@ -0,0 +1,29 @@
+import {
+  GET_MENTORS_queryDataSchema,
+  TGET_MENTORS_queryDataSchema as IMentorAPIReponse,
+} from "services/apollo/queries/queries-properties";
+import { z } from "zod";
+
+export const mentorClientSchema = GET_MENTORS_queryDataSchema.pick({
+  description: true,
+  firstName: true,
+  id: true,
+  jobTitle: true,
+  lastName: true,
+  __typename: true,
+})
+  .extend({
+    image: z.object({
+      blurDataURL: z.string().optional(),
+      blurHeight: z.number().optional(),
+      blurWidth: z.number().optional(),
+      height: z.number(),
+      width: z.number(),
+      src: z.string(),
+    }),
+    chips: z.array(z.string()).nullable(),
+    location: z.string(),
+  })
+  .strict();
+
+export type IMentorClient = z.infer<typeof mentorClientSchema>;
--- src/services/apollo/queries/queries-properties/index.ts
+++ src/services/apollo/queries/queries-properties/index.ts
@@ -3,3 +3,4 @@ export * from "./GET_ME";
 export * from "./GET_MENTOR_BY_ID";
 export * from "./GET_SKILLS";
 export * from "./GET_EVENTS";
+export * from "./GET_MENTORS";
