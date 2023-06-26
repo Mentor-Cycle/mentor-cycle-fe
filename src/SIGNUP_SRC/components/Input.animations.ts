dababc731ad58e54da093cdb77c2126581d05659
Vitor Markis <vitormarkis2369@gmail.com>
2023-06-26 15:40:16 -0300

    refactor: remove old Input files

--- src/SIGNUP_SRC/components/Input.animations.ts
+++
@@ -1,5 +0,0 @@
-export const errorAnimation = () => ({
-  initial: { y: -10, opacity: 0 },
-  animate: { y: 0, opacity: 1, transition: { duration: 0.1 } },
-  exit: { x: -10, opacity: 0, transition: { duration: 0.1 } },
-});
