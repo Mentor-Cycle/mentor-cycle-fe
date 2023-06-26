e1a347285e82198c3841de5c0146fc3939afcb23
Vitor Markis <vitormarkis2369@gmail.com>
2023-06-26 17:08:07 -0300

    refactor: switch country input to select + refactor Input

--- src/SIGNUP_SRC/steps/Location.tsx
+++ src/SIGNUP_SRC/steps/Location.tsx
@@ -1,97 +1,156 @@
-import { Input } from "SIGNUP_SRC/components/Input";
+import {
+  Input,
+  InputErrorMessage,
+  InputLabel,
+  InputWrapper,
+} from "SIGNUP_SRC/components/Input";
 import { IFormValues } from "SIGNUP_SRC/types";
 import { Controller, useFormContext } from "react-hook-form";
-import { motion, AnimatePresence } from "framer-motion";
-import { errorAnimation } from "SIGNUP_SRC/components/Input.animations";
-import { PatternFormat } from "react-number-format";
-import { Select } from "SIGNUP_SRC/components/SelectControlled";
+import {
+  InputAttributes,
+  PatternFormat,
+  PatternFormatProps,
+} from "react-number-format";
+import { Select as MultiSelect } from "SIGNUP_SRC/components/SelectControlled";
+import { useTypedQuery } from "@hooks/useTypedQuery";
+import { queriesIndex as api } from "services/apollo/queries/queries.index";
+import React, { useId, useState } from "react";
+import Select from "react-select";
+import { useGeoCallbacks } from "SIGNUP_SRC/hooks/useGeoCallbacks";
+import { MultipleInputsContainer } from "SIGNUP_SRC/components/Input/MultipleInputsContainer";
+import { ReactSelectInterface } from "types/react-select";
+import { CountrySelector } from "SIGNUP_SRC/steps/components/CountrySelector";
+import { IPaisesIBGESchema } from "SIGNUP_SRC/schemas/paises";
+import { IEstadosIBGESchema } from "SIGNUP_SRC/schemas/estados";
 
 export const Location = () => {
+  const [countries, setCountries] = useState<IPaisesIBGESchema | null>(null);
+  const [states, setStates] = useState<IEstadosIBGESchema | null>(null);
+  const skillsId = useId();
+
+  useGeoCallbacks("paises", setCountries, console.error);
+  useGeoCallbacks("estados", setStates, console.error);
+
   const {
     register,
     control,
     formState: { errors },
+    watch,
   } = useFormContext<IFormValues>();
 
-  const passwordErrors =
-    errors.password?.message ?? errors.repeatPassword?.message;
+  const { data: skillsResponse, error: errorSkillsResponse } = useTypedQuery(
+    api.GET_SKILLS
+  );
+  if (errorSkillsResponse?.error) {
+    console.log("errorSkillsResponse", errorSkillsResponse);
+  }
+
+  const skillsOptions =
+    skillsResponse?.findAllSkills.map((skill) => skill.name) ?? null;
+
+  const countriesOptions: ReactSelectInterface[] | null =
+    countries?.map(({ nome }) => ({
+      value: nome,
+      label: nome,
+    })) ?? null;
+
+  const country = watch("country");
+  console.log("Country", country);
 
   return (
     <>
-      <div className="flex gap-2">
-        <Input
-          {...register("country")}
-          errorMessage={errors.country?.message}
-          label="País:"
-          placeholder="Brasil"
-          autoFocus
-        />
+      <MultipleInputsContainer>
+        <InputWrapper grow={1}>
+          <InputLabel label="País:" />
+          <Controller
+            name="country"
+            control={control}
+            render={({ field }) => (
+              <CountrySelector
+                field={field}
+                countriesOptions={countriesOptions}
+              />
+            )}
+          />
+          <InputErrorMessage errorMessage={errors.country?.message} />
+        </InputWrapper>
         <Input
           {...register("state")}
           errorMessage={errors.state?.message}
           label="Estado:"
           placeholder="SP"
         />
-      </div>
-      <div className="flex gap-2">
+      </MultipleInputsContainer>
+      <MultipleInputsContainer>
         <Input
           {...register("city")}
           errorMessage={errors.city?.message}
           label="Cidade:"
           placeholder="Campinas"
         />
-        <Controller
-          name="birthDate"
-          control={control}
-          render={({ field }) => (
-            <Input
-              asChild
-              label="Data de Nascimento:"
-              errorMessage={errors.birthDate?.message}
-              placeholder="__/__/____"
-              defaultValue={field.value}
-              onChange={(e) => {
-                const newEvent = e.target.value.replace(/\D/g, "");
-                field.onChange(newEvent);
-              }}
-              onBlur={field.onBlur}
-            >
-              <PatternFormat
+
+        <InputWrapper grow={1}>
+          <InputLabel label="Data de Nascimento:" />
+          <Controller
+            name="birthDate"
+            control={control}
+            render={({ field: { value, ...rest } }) => (
+              <DateInput
                 valueIsNumericString
                 format="##/##/####"
                 mask="_"
+                placeholder="__/__/____"
+                defaultValue={value.replace(/\D/g, "")}
+                className="input-sign"
+                {...rest}
               />
-            </Input>
-          )}
-        />
-      </div>
-      <Input
-        asChild
-        errorMessage={errors.skills?.message}
-        label="Especialização:"
-      >
+            )}
+          />
+          <InputErrorMessage errorMessage={errors.birthDate?.message} />
+        </InputWrapper>
+      </MultipleInputsContainer>
+
+      <InputWrapper grow={1}>
+        <InputLabel label="Especializações:" />
         <Controller
           name="skills"
           control={control}
-          render={(props) => {
+          render={({ field }) => {
             return (
-              <Select
-                options={[
-                  "QA",
-                  "Devops",
-                  "Front-End",
-                  "UI/UX Design",
-                  "Full-Stack",
-                  "Product Design",
-                  "Back-End",
-                ]}
+              <MultiSelect
+                id={skillsId}
+                options={skillsOptions}
                 tabIndex={20}
-                {...props.field}
+                className="input-sign"
+                {...field}
+                ref={null}
               />
             );
           }}
         />
-      </Input>
+        <InputErrorMessage errorMessage={errors.skills?.message} />
+      </InputWrapper>
     </>
   );
 };
+
+export interface IDateInput extends PatternFormatProps<InputAttributes> {}
+
+export const DateInput = React.forwardRef<HTMLInputElement, IDateInput>(
+  function DateInputComponent({ className, format, ...rest }, ref) {
+    const _cn = className ? ` ${className}` : "";
+
+    return (
+      <>
+        <input type="hidden" ref={ref} />
+        <PatternFormat
+          valueIsNumericString
+          format="##/##/####"
+          mask="_"
+          className={"" + _cn}
+          {...rest}
+        />
+      </>
+    );
+  }
+);
---
+++ src/SIGNUP_SRC/steps/components/CountrySelector.tsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { Noop, RefCallBack } from "react-hook-form";
+import Select, { SingleValue } from "react-select";
+import { ReactSelectInterface } from "types/react-select";
+import { IFormValues } from "SIGNUP_SRC/types";
+
+export type ControllerRenderProps = {
+  onChange: (...event: any[]) => void;
+  onBlur: Noop;
+  value: string;
+  name: keyof IFormValues;
+  ref: RefCallBack;
+};
+
+interface CountrySelectorProps {
+  countriesOptions: ReactSelectInterface[] | null;
+  field: ControllerRenderProps;
+}
+
+export function CountrySelector(props: CountrySelectorProps) {
+  const brasilOption: ReactSelectInterface = {
+    label: "Brasil",
+    value: "Brasil",
+  };
+
+  const defaultValue = brasilOption;
+
+  const actualValue: ReactSelectInterface = {
+    label: props.field.value,
+    value: props.field.value,
+  };
+
+  function handleOnChange(newValue: SingleValue<ReactSelectInterface>) {
+    if (newValue) props.field.onChange(newValue.value);
+  }
+
+  return (
+    <Select
+      options={props.countriesOptions ?? []}
+      autoFocus
+      unstyled
+      value={actualValue}
+      defaultValue={defaultValue}
+      onChange={handleOnChange}
+      onBlur={props.field.onBlur}
+      styles={{
+        control: (base) => ({
+          ...base,
+          minHeight: "unset",
+          cursor: "pointer",
+        }),
+        menu: (base, props) => {
+          return {
+            ...base,
+            left: "-1px",
+            right: "-1px",
+            transform: "translateY(-0.5rem)",
+            borderRadius: "0 0 0.5rem 0.5rem",
+            borderTop: "none",
+            width: "unset",
+          };
+        },
+        menuList: (base) => ({
+          ...base,
+          maxHeight: "18rem",
+        }),
+      }}
+      classNames={{
+        container: () => "input-sign",
+        menu: () => "input-sign",
+        option: () =>
+          "py-2 px-4 hover:bg-secondary-02 rounded-lg hover:cursor-pointer",
+      }}
+    />
+  );
+}
