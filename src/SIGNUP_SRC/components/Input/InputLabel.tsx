fc7b2605edc70e2ce4706c9aaa95cf77d6c994b8
Vitor Markis <vitormarkis2369@gmail.com>
2023-06-27 16:34:28 -0300

    feat: add blocked style when input is disabled

--- src/SIGNUP_SRC/components/Input/InputLabel.tsx
+++ src/SIGNUP_SRC/components/Input/InputLabel.tsx
@@ -2,13 +2,21 @@ import React, { LabelHTMLAttributes } from "react";
 
 interface InputLabelProps extends LabelHTMLAttributes<HTMLLabelElement> {
   label: string;
+  disabled?: boolean;
 }
 
-export function InputLabel({ label, className, ...rest }: InputLabelProps) {
+export function InputLabel({ disabled, label, className, ...rest }: InputLabelProps) {
   const _cn = className ? ` ${className}` : "";
 
   return (
-    <label className={"mb-1 text-sm text-gray-03" + _cn} {...rest}>
+    <label
+      data-disabled={disabled}
+      className={
+        `mb-1 text-sm text-gray-03 data-[disabled=true]:text-gray-02 data-[disabled=true]:cursor-not-allowed` +
+        _cn
+      }
+      {...rest}
+    >
       {label}
     </label>
   );
--- src/SIGNUP_SRC/components/Input/InputWrapper.tsx
+++ src/SIGNUP_SRC/components/Input/InputWrapper.tsx
@@ -3,9 +3,11 @@ import React, { CSSProperties, HTMLAttributes } from "react";
 interface InputWrapperProps extends HTMLAttributes<HTMLDivElement> {
   grow?: number;
   children: React.ReactNode;
+  disabled?: boolean;
 }
 
 export function InputWrapper({
+  disabled,
   grow,
   children,
   className,
@@ -17,8 +19,10 @@ export function InputWrapper({
 
   return (
     <div
+      data-disabled={disabled}
       className={
-        "flex flex-col outline-none grow-[var(--grow)] min-w-0 basis-0" + _cn
+        "flex flex-col outline-none grow-[var(--grow)] min-w-0 basis-0 data-[disabled=true]:cursor-not-allowed" +
+        _cn
       }
       style={{ "--grow": grow, ..._st } as CSSProperties}
       {...rest}
