485bbbea4b8c1fd1d6bbb6d796872ce8df77440c
Leonardo <oliveirabalsa2@gmail.com>
2023-07-03 01:18:23 -0300

    feat(modalNotifications): add event status translation

--- src/components/Modal/ModalNotifications/ModalNotifications.tsx
+++ src/components/Modal/ModalNotifications/ModalNotifications.tsx
@@ -3,11 +3,23 @@ import { useUser } from "@hooks/useUser";
 import { useModal } from "contexts/ModalContext";
 import Modal from "..";
 import { ModalActionTypes } from "contexts/types";
+import { eventStatusToPortugueseMap } from "utils/parser/eventStatusToPortuguese";
+import { OptionStatus } from "schemas/create_event_output";
+import { tr } from "date-fns/locale";
 
 const ModalNotifications = () => {
   const { user } = useUser();
   const { closeModal, NOTIFICATIONS_MODAL } = useModal();
   const { notifications } = user;
+
+  const getTranslatedText = (text = "") => {
+    const [eventStatusInEnglish] = Object.keys(
+      eventStatusToPortugueseMap
+    ).filter((key) => text.includes(key));
+    const eventStatusInPortuguese =
+      eventStatusToPortugueseMap[eventStatusInEnglish as OptionStatus];
+    return text.replace(eventStatusInEnglish, eventStatusInPortuguese);
+  };
 
   return (
     <Modal
@@ -25,7 +37,9 @@ const ModalNotifications = () => {
               .map((notification) => (
                 <CardNotification
                   key={notification.id}
-                  description={notification.data?.description || ""}
+                  description={getTranslatedText(
+                    notification.data?.description
+                  )}
                   name={notification.data?.title || ""}
                   imgUrl={notification.data?.imageUrl || ""}
                   alreadyViewed={notification.read}
