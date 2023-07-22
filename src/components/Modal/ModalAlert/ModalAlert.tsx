c2f0e6b141e42168da1ffb299e39fb069f946bc2
Ar3secchim <renarasecchim@gmail.com>
2023-07-22 10:08:33 -0300

    feat: update style modalAlert

--- src/components/Modal/ModalAlert/ModalAlert.tsx
+++ src/components/Modal/ModalAlert/ModalAlert.tsx
@@ -10,10 +10,8 @@ const ModalAlert = () => {
       onOpenChange={() => closeModal(ModalActionTypes.ALERT_MODAL)}
     >
       <Modal.ContentAlert>
-        <div className="max-xl:px-5 py-16 w-[300px] xs:w-[380px] sm:w-[600px] md:w-auto p-2 lg:px-20 ">
-          <Modal.Title text="Tem certeza que deseja mudar a foto do perfil?" />
-          <Modal.Description text=" " />
-        </div>
+        <Modal.Title text="Tem certeza que deseja mudar a foto do perfil?" />
+        <Modal.Description text=" " />
       </Modal.ContentAlert>
     </Modal.Root>
   );
--- src/components/Modal/ModalRoot/ModalContentAlert.tsx
+++ src/components/Modal/ModalRoot/ModalContentAlert.tsx
@@ -1,13 +1,13 @@
+import Button from "@components/Button";
 import * as Dialog from "@radix-ui/react-dialog";
 import { ModalRootProps } from "./Modal.types";
-import Button from "@components/Button";
 
 const ModalContentAlert = ({ children }: ModalRootProps) => {
   return (
-    <Dialog.Content className="text-center py-6 text-secondary-03 z-50 rounded-lg bg-scroll overflow-y-auto bg-neutral-01 dark:bg-secondary-02 fixed top-[50%] left-[50%] max-h-[90vh] w-auto translate-x-[-50%] translate-y-[-50%]  shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] ">
-      <div className="flex flex-col items-center">
+    <Dialog.Content className="fixed left-[50%] top-[50%] z-50 max-h-[90vh] w-auto translate-x-[-50%] translate-y-[-50%] overflow-y-auto rounded-lg bg-neutral-01 bg-scroll py-16 text-center text-secondary-03 shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px]  dark:bg-secondary-02 ">
+      <div className="flex w-[350px] flex-col items-center gap-7 max-xl:px-5 xs:w-[380px] sm:w-[600px] lg:px-20">
         {children}
-        <div className="flex gap-6 w-[300px] xs:w-[380px] sm:w-[600px] md:w-[60%]">
+        <div className="flex w-full gap-6">
           <Button type="submit" variant="primary" size="small">
             Sim, pode mudar
           </Button>
